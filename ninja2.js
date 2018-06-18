33071328eff7117e6f6d91264d99cbb80541a116
Nayyer <nayyermhaseem@gmail.com>
2018-06-18 20:38:52 +0300

    ninja2

---
+++ ninja2.js
@@ -0,0 +1,65 @@
+//Private variables are scoped to the constructor with the 'var' keyword
+function Ninja(name) {
+    if (!(this instanceof Ninja)) {
+        console.log('I am a not ninja', name);
+        return new Ninja(name);
+      }
+    var speed = 3;
+    var strength = 3;
+    this.name = name;
+    this.health = 100;
+    
+    // To make functions private, we scope them to the constructor
+ 
+    this.showStrength = function(){
+    return strength;
+
+    }
+    this.showSpeed = function(){
+    return speed;
+    }
+    this.showStats = function() {
+        console.log('my name is',this.name);
+        console.log('my speed is',this.showSpeed());
+        console.log('my health is',this.health);
+        console.log('my strength is ', this.showStrength());
+    return this;
+    }
+}    
+Ninja.prototype.drinkSake = function(){
+       this.health += 10;
+    return this;
+   }
+
+
+Ninja.prototype.sayName = function(){
+    
+        console.log('My name is ',this.name);
+  
+
+
+    }
+
+Ninja.prototype.punch = function(target){
+    if (!target)
+    { throw new Error ("I am not going to punch in the air")}
+   target.health -= 10;
+   console.log (this.name,'punched',target.name,'and his health is reduced to',target.health)
+    }
+
+
+Ninja.prototype.kick = function(target){
+    if (!target)
+    { throw new Error ("I am not going to kick in the air")}
+   target.health -= 15;
+   console.log (this.name,'kicked',target.name,'and his health is reduced to',target.health)
+ 
+    }   
+
+var turtle = new Ninja('Dell');
+var turtle1 = new Ninja('Gates');
+turtle1.drinkSake().showStats();
+turtle.punch(turtle1);
+turtle.kick(turtle1);
+
+// How can i chain the methods, punch().kick()?
